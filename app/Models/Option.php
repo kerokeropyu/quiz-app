5acbec498254e1fd7fa1043fdfb2be0fb0481a62
root <root@DESKTOP-NKRS2A9>
2024-10-04 19:17:51 +0900

    リレーションの設定

--- app/Models/Option.php
+++ app/Models/Option.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Option extends Model
 {
     use HasFactory;
+
+    public function quiz(): BelongsTo
+    {
+        return $this->belongsTo(Quiz::class);
+    }
 }
